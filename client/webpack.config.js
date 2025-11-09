9bd3ab4d5eb45b294f8ba75da9730b655691c5b2
Pratik Bavche <pratikbavche2005@gmail.com>
2025-11-09 16:04:06 +0530

    Add files via upload

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,5 +25,19 @@ module.exports = {
   ignoreWarnings: [
     /Failed to parse source map/,
     /Module not found: Error: Can't resolve 'fs'/,
+    /Can't resolve 'fs'/,
+    /Can't resolve 'fs' in/,
+    /node_modules\/face-api\.js/,
+    /face-api\.js.*fs/,
+    {
+      module: /node_modules\/face-api\.js/,
+    },
+    (warning) => {
+      if (warning.message) {
+        return warning.message.includes("Can't resolve 'fs'") || 
+               warning.message.includes("face-api.js");
+      }
+      return false;
+    },
   ],
 };
